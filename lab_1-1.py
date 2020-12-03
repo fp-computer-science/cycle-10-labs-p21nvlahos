f90951b20a8a67db356fb42880141f847635d0d4
Niko <p21nvlahos@fairfieldprep.org>
2020-12-03 10:18:23 -0500

    Add lab1-1 and lab1-2

---
+++ lab_1-1.py
@@ -0,0 +1,8 @@
+# Author: NV 12/2/2020
+
+def greeting():
+    '''This function printss "Hello World!"'''
+    print("Hello World!")
+
+help(greeting)
+greeting()
---
+++ lab_1-2.py
@@ -0,0 +1,9 @@
+# Author: NV 12/2/2020
+
+def find_sum(num1, num2):
+    '''Adds the 2 numbeers togethher'''
+    num_sum = num1 + num2
+    return num_sum
+
+my_sum = find_sum(111, 222)
+print(my_sum)
